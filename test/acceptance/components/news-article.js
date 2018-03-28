89e7fb252a5fbc451a8ac9b42c760417270d6bcd
Tom Dale <tdale@tys-its-135884.usatoday.us.ad.gannett.com>
2018-03-28 16:03:22 -0400

    issue-25 fixed broken acceptence tests

--- test/acceptance/components/news-article.js
+++ test/acceptance/components/news-article.js
@@ -86,7 +86,7 @@ module.exports = {
                 displayed: true,
               },
               xCloseButton: {
-                displayed: false,
+                displayed: true,
               },
             },
             options: {
