76ce812da749442cc021b34076e8ff6394e27218
Tom Dale <tdale@gannett.com>
2018-04-19 11:39:23 -0400

    issue-57-4 initial commit

--- test/unit/lib/util/page-state-handler-tests.js
+++ test/unit/lib/util/page-state-handler-tests.js
@@ -4,7 +4,7 @@ const mockery = require('mockery');
 const sinon = require('sinon');
 const expect = require('chai').expect;
 
-describe('lib/util/page-state-handler.js', function() {
+describe.only('lib/util/page-state-handler.js', function() {
   describe('on file being required', function() {
     let pageStateHandler;
     let EventEmitter;
@@ -129,6 +129,86 @@ describe('lib/util/page-state-handler.js', function() {
           ['pageStateHandler.componentRetrieved', 'component1', {pageModel: {}, length: 2, callback}],
           ['pageStateHandler.componentRetrieved', 'component2', {pageModel: {}, length: 2, callback}],
         ]);
+      });
+    });
+  });
+
+  describe('_getComponentState', function() {
+    let EventEmitter;
+    let EventEmitterInstance;
+    let pageStateHandler;
+    let getElementData;
+    let driver;
+    let component;
+    let pageStateData;
+
+    beforeEach(function() {
+      mockery.enable({useCleanCache: true});
+      mockery.registerAllowable('../../../../lib/util/page-state-handler.js');
+
+      EventEmitter = sinon.stub();
+      EventEmitterInstance = {
+          emit: sinon.stub(),
+          on: sinon.stub(),
+      };
+      EventEmitter.returns(EventEmitterInstance);
+
+      getElementData = sinon.stub();
+
+      driver = {
+        executeAsyncScript: sinon.stub().resolves({data: 'someData'}),
+      };
+      global.driver = driver;
+
+      component = {
+        type: 'Component',
+        name: 'component1',
+        elements: ['someElement1', 'someElement2'],
+        model: {someModel: 'ofThisComponent'},
+      };
+
+      pageStateData = {
+        state1: 'someData',
+        state2: 'someOtherData',
+      };
+
+      mockery.registerMock('events', {EventEmitter});
+      mockery.registerMock('lodash', {});
+      mockery.registerMock('./get-element-data.js', getElementData);
+
+      pageStateHandler = require('../../../../lib/util/page-state-handler.js');
+    });
+
+    afterEach(function() {
+      delete global.driver;
+      mockery.resetCache();
+      mockery.deregisterAll();
+      mockery.disable();
+    });
+
+    it('should call driver.executeAsyncScript with getElementData and the passed in component.elements', function() {
+      pageStateHandler._getComponentState(component, pageStateData);
+
+      expect(driver.executeAsyncScript.args).to.deep.equal([
+        [getElementData, component.elements],
+      ]);
+    });
+
+    describe(`when driver.executeAsyncScript's promise is resolved`, function() {
+      it.only(`should call pageStateHandler.emit with the event 'pageStateHandler.componentDataRecieved' `
+      + `the component.model, the component, the returned data, and the passed in pageStateData`, function() {
+        pageStateHandler._getComponentState(component, pageStateData);
+
+        expect(pageStateHandler.emit.callCount).to.equal(1);
+        // expect(pageStateHandler.emit.args).to.deep.equal([
+        //   [
+        //     'pageStateHandler.componentDataRecieved',
+        //     component.model,
+        //     component,
+        //     {data: 'someData'},
+        //     pageStateData,
+        //   ],
+        // ]);
       });
     });
   });
