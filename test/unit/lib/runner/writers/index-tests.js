21b6ca156d1ceed7a942f022ec73541fabeef260
Subarna Mukhiyabeen <smukhiyabe@gannett.com>
2018-10-01 11:34:23 -0400

    new branch for action reporter

--- test/unit/lib/runner/writers/index-tests.js
+++ test/unit/lib/runner/writers/index-tests.js
@@ -6,14 +6,17 @@ const expect = require('chai').expect;
 
 describe('lib/runner/writers/index.js', function() {
   let json;
+  let actionJson;
 
   beforeEach(function() {
     mockery.enable({useCleanCache: true});
     mockery.registerAllowable('../../../../../lib/runner/writers');
 
     json = sinon.stub();
+    actionJson = sinon.stub();
 
     mockery.registerMock('./write-json-to-disk.js', json);
+    mockery.registerMock('./action-json-writer.js', actionJson);
   });
 
   afterEach(function() {
@@ -22,10 +25,10 @@ describe('lib/runner/writers/index.js', function() {
     mockery.disable();
   });
 
-  it('should export 1 item on an object', function() {
+  it('should export 2 items on an object', function() {
     let result = require('../../../../../lib/runner/writers');
 
-    expect(Object.getOwnPropertyNames(result).length).to.equal(1);
+    expect(Object.getOwnPropertyNames(result).length).to.equal(2);
   });
 
   it('should have the property \'JSON\' with the value from requiring'
@@ -33,5 +36,12 @@ describe('lib/runner/writers/index.js', function() {
     let result = require('../../../../../lib/runner/writers');
 
     expect(result.JSON).to.deep.equal(json);
+  });
+
+  it('should have the property \'actionJSON\' with the value from requiring'
+    + ' \'./action-json-writer.js\'', function() {
+    let result = require('../../../../../lib/runner/writers');
+
+    expect(result.actionJSON).to.deep.equal(actionJson.write);
   });
 });
