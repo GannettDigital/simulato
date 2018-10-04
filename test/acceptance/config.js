8d4892d04fa99081c9646d4488fc54649b6c0111
Tom Dale <tdale@gannett.com>
2018-10-04 15:59:19 -0400

    issue-167: updated test config

--- test/acceptance/config.js
+++ test/acceptance/config.js
@@ -7,11 +7,4 @@ module.exports = {
   outputPath: './test/acceptance/tests',
   plannerAlgorithm: 'forwardStateSpaceSearchHeuristic',
   rerunFailedTests: 0,
-  // driver: {
-  //   saucelabs: false,
-  //   capabilities: {
-  //     browserName: 'chrome',
-  //   },
-  //   // usingServer: 'http://test-infrastructure:3BDE8FP7D2zq5fFrUjAjAtsS@10.155.236.62:4444/wd/hub',
-  // },
 };
